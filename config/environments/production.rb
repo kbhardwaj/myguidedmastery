c43852ceed2480e806d93c576b47f73bf990b935
Kaustubh Bhardwaj <kaustubh.bhardwaj86@gmail.com>
2013-05-29 10:57:40 -0400

    added assets precompile line to production rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Myguidedmastery::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += %w( bootstrap.min.js )
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
