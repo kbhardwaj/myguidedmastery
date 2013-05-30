e5705df7be583d3ab1c2f324db8e7f6cb11f9bfd
Kaustubh Bhardwaj <kaustubh.bhardwaj86@gmail.com>
2013-05-30 18:55:34 -0400

    tweaked asset compilation, turned off asset compile in production and precompiled all assets after deleting public assets once

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Myguidedmastery::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   
 
