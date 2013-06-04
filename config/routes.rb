792ad770ead3d75d3f7e19d41c79f038f74a21f9
Kaustubh Bhardwaj <kaustubh.bhardwaj86@gmail.com>
2013-06-04 17:08:32 -0400

    moved root route above activeadmin statement in routes rb to solve uninitialized constant dashboardscontroller problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Myguidedmastery::Application.routes.draw do
+  
+  root :to => 'pages#home'
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -13,6 +16,8 @@ Myguidedmastery::Application.routes.draw do
   resources :notebooks do
     resources :invites
   end
+
+
 
   devise_for :users
   ActiveAdmin.routes(self)
@@ -66,7 +71,7 @@ Myguidedmastery::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+  
   
   match '/contact', to: 'pages#contact'
   match '/about', to: 'pages#about'
