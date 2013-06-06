1d6aafa653a2ff60d00c8d099b5dbdd417976bba
Kaustubh Bhardwaj <kaustubh.bhardwaj86@gmail.com>
2013-06-05 23:46:22 -0400

    edited production settings for mailer and edited setup mail file for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Myguidedmastery::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'myguidedmastery.com' }
 
   # Enable threaded mode
   # config.threadsafe!
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "railscasts.com",
-  :user_name            => "railscasts",
-  :password             => "secret",
+  :domain               => "myguidedmastery.com",
+  :user_name            => ENV["GMAIL_USERNAME"],
+  :password             => ENV["GMAIL_PASSWORD"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
